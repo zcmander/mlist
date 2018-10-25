065e08e1e9ab311760cac1d32ddf0eefa2bee99b
Teijo Mursu <zcmander@gmail.com>
2018-10-25 05:54:27 +0300

    Replace requirements.txt with setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='mlist',
+      version='0.41',
+      install_requires=[          
+        'django',
+        'django-crispy-forms',
+        'django-ember',
+        'django-tastypie',
+        'django-taggit',
+        'django-debug-toolbar',
+        'requests',
+        'Whoosh',
+        'mimeparse',
+        'pytz',
+        'django-haystack',
+        'simplejson',
+      ],
+      packages=find_packages(),
+      scripts=['manage.py'])
+
