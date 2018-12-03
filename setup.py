61ca4c4de142634ae80692ced07a48d443609567
Teijo Mursu <zcmander@gmail.com>
2018-12-03 21:35:23 +0200

    Split requirements to external requirements.txt so Azure Web App can automatically install dependecies in deployment.

--- setup.py
+++ setup.py
@@ -1,32 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='mlist',
       version='0.41',
-      install_requires=[
-          # Django
-          'django',
-          'django-crispy-forms',
-          'django-tastypie',
-          'django-taggit',
-          'django-debug-toolbar',
-          'django-haystack',
-
-          # Other required libraries
-          'requests',
-          'Whoosh',
-          'mimeparse',
-          'pytz',
-          'simplejson',
-          'unicodecsv',
-
-          # Code quality
-          'flake8',
-          'mock',
-          'coverage',
-
-          # CI/CD
-          'coveralls'
-      ],
+      install_requires=requirements,
       packages=find_packages(),
       scripts=['manage.py'])
