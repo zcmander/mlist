f10094c259da50e5d2e0264bda6068608a987d91
Teijo Mursu <zcmander@gmail.com>
2019-08-13 22:36:11 +0300

    Upgraded from bootstrap 2 to bootstrap4. Removed modernizer. Upgraded crips form and the mainmenu to use bootstrap 4 theme (other pages can wait for now).

--- mlistsite/settings.py
+++ mlistsite/settings.py
@@ -66,17 +66,17 @@ SHORT_DATETIME_FORMAT = "j.n.Y H:i:s"
 DATETIME_FORMAT = r"j. E Y k\l\o H:i:s"
 
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "dist")
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -91,6 +91,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
@@ -129,13 +130,23 @@ INSTALLED_APPS = (
     'taggit',
     'crispy_forms',
     'haystack',
-    'debug_toolbar'
+    'debug_toolbar',
+    'sass_processor'
 )
 
 USE_THOUSAND_SEPARATOR = False
 
 INTERNAL_IPS = ('127.0.0.1',)
-CRISPY_TEMPLATE_PACK = 'bootstrap'
+
+SASS_PROCESSOR_INCLUDE_DIRS = [
+    os.path.join(BASE_DIR, 'node_modules'),
+]
+
+print(SASS_PROCESSOR_INCLUDE_DIRS)
+
+SASS_PRECISION = 8
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
