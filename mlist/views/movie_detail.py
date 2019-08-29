80840fd92b1e214c789acbc1f025436a18430fc7
Teijo Mursu <zcmander@gmail.com>
2019-08-29 22:29:52 +0300

    Converted part of movie details page to use attributes (combined info of backend movie attributes).

--- mlist/views/movie_detail.py
+++ mlist/views/movie_detail.py
@@ -5,6 +5,20 @@ from django.utils.decorators import method_decorator
 from mlist.models import MovieInCollection, IMDBMovie, TMDBMovie, BackendMovieAttribute
 
 
+class MovieAttributes:
+    def __init__(self, attributes):
+        self._data = attributes
+
+    def __getattr__(self, key):
+        return self._data[key]
+
+    def has(self, key):
+        return key in self._data
+
+    def get(self, key):
+        return self._data.get(key)
+
+
 @method_decorator(login_required, name='dispatch')
 class MovieDetail(DetailView):
     model = MovieInCollection
@@ -29,8 +43,57 @@ class MovieDetail(DetailView):
             .filter(backend_movie__movie=self.object.movie).select_subclasses().all()
 
         # Convert to dict
-        context["attributes"] = {}
+        attributes_dict = {}
         for attribute in attributes:
-            context["attributes"][attribute.key] = attribute.value
+            attributes_dict[attribute.key] = attribute.value
+
+        context["has_poster"] = any([
+            'imdb.poster_url' in attributes_dict,
+            'tmdb.poster_path' in attributes_dict
+        ])
+        context["has_backdrop"] = any([
+            'tmdb.backdrop_path' in attributes_dict,
+        ])
+
+        context["has_overview"] = any([
+            attr in attributes_dict
+            for attr in [
+                'released',
+                'runtime',
+                'genres',
+                'imdb.rating',
+                'tmdb.vote_average',
+            ]])
+
+        detail_keys = [
+            'director'
+        ]
+
+        context["has_details"] = any([
+            attr in attributes_dict
+            for attr in detail_keys])
+
+        context["has_share"] = any([
+            'imdb_id' in attributes_dict,
+        ])
+
+        if 'title' not in attributes_dict:
+            attributes_dict["title"] = self.object.movie.title
+
+        movie_attributes = MovieAttributes(attributes_dict)
+        context['attributes'] = movie_attributes
+
+        context['details'] = [
+            ("Director", movie_attributes.director, 'text'),
+            ("Writer", movie_attributes.writer, 'brlist'),
+            ("Actors", movie_attributes.actors, 'brlist'),
+            ("Companies", movie_attributes.production_companies, 'brlist'),
+            ("Spoken languages", movie_attributes.spoken_languages, 'brlist'),
+            ("Homepage", movie_attributes.homepage, 'link'),
+            ("Budget", movie_attributes.budget, 'intcomma'),
+            ("Revenue", movie_attributes.revenue, 'intcomma'),
+        ]
+
+        context['debug'] = sorted(attributes_dict.items())
 
         return context
