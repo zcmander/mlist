db33b5be5efb4c2194aa22ce9f920bdcb8322755
Teijo Mursu <zcmander@gmail.com>
2019-11-23 22:26:15 +0200

    Added comments to clear up complex request context.

--- mlist/views/movie_detail.py
+++ mlist/views/movie_detail.py
@@ -13,6 +13,12 @@ DisplayAttribute = namedtuple('DisplayAttribute', [
 
 
 class MovieAttributes:
+    """
+    Movie attributes container
+
+    Allows templates to use keys as attributes,
+    like "attributes.title" to show movie title.
+    """
     def __init__(self, attributes):
         self._data = attributes
 
@@ -52,23 +58,26 @@ class MovieDetail(DetailView):
         for attribute in attributes:
             attributes_dict[attribute.key] = attribute.value
 
+        # Make sure that arributes has title
         if 'title' not in attributes_dict:
             attributes_dict["title"] = movie.title
 
         movie_attributes = MovieAttributes(attributes_dict)
         context['attributes'] = movie_attributes
 
+        # Poster
         context["has_poster"] = any([
             'imdb.poster_url' in attributes_dict,
             'tmdb.poster_path' in attributes_dict
         ])
-
         context["poster_url"] = movie.poster_url
 
+        # Backdrop
         backdrop_url = movie.backdrop_url
         context["has_backdrop"] = backdrop_url is not None
         context["backdrop_url"] = backdrop_url
 
+        # Overview
         context["has_overview"] = any([
             attr in attributes_dict
             for attr in [
@@ -78,10 +87,6 @@ class MovieDetail(DetailView):
                 'imdb.rating',
                 'tmdb.vote_average',
             ]])
-
-        context["has_share"] = any([
-            'imdb_id' in attributes_dict,
-        ])
 
         imdb_rating = None
         if 'imdb.rating' in attributes_dict:
@@ -99,10 +104,16 @@ class MovieDetail(DetailView):
             }
         context['tmdb_rating'] = tmdb_rating
 
+        # Share
+        context["has_share"] = any([
+            'imdb_id' in attributes_dict,
+        ])
+
         if 'imdb_id' in attributes_dict:
             context['imdb_url'] = 'http://imdb.com/title/' + attributes_dict['imdb_id']
             context['facebook_share_url'] = 'http://www.facebook.com/sharer.php?u=' + context['imdb_url']
 
+        # Details
         detail_display = OrderedDict([
             ["director", DisplayAttribute("Director", 'text')],
             ["writer", DisplayAttribute("Writer", 'brlist')],
@@ -124,7 +135,5 @@ class MovieDetail(DetailView):
             detail_display[key].type
         ) for key in detail_display.keys()
           if key in attributes_dict]
-
-        # context['debug'] = sorted(attributes_dict.items())
 
         return context
